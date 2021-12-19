92aeeabfaea6a36a429127150e46a9ccf1abb3ab
Aviroute <youturncorporation@gmail.com>
2021-12-19 15:17:05 -0500

    adding

--- src/components/ui-components/Alert.js
+++ src/components/ui-components/Alert.js
@@ -1,6 +1,6 @@
 const Alert = (props) => {
     return(
-        <div class={`alert ${props.alertType}`} role="alert">
+        <div className={`alert ${props.alertType}`} role="alert">
         {props.alertMessage}
         </div>
     );
