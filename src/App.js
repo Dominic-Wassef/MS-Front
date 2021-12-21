15a4cceeebce8a2bf7cd532a3aa388db94e5631f
Aviroute <youturncorporation@gmail.com>
2021-12-21 14:51:25 -0500

    adding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Movies from './components/Movies';
 import Admin from './components/Admin';
@@ -8,15 +8,29 @@ import Genres from './components/Genres';
 import OneGenre from './components/OneGenre';
 import EditMovie from './components/EditMovie';
 
-export default function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jwt: "",
+    }
+    this.handleJWTChange(this.handleJWTChange(this));
+  }
+
+  handleJWTChange = (jwt) => {
+    this.setState({jwt: jwt});
+  }
+
+  logout = () => {
+    this.setState({jwt: ""});
+  }
+
+  render() {
   return (
     <Router>
     <div className="container">
-
       <div className="row">
-        <h1 className="mt-3">
-          Go Watch a Movie!
-        </h1>
+        <h1 className="mt-3">Go Watch a Movie!</h1>
         <hr className="mb-3"></hr>
       </div>
 
@@ -72,4 +86,5 @@ export default function App() {
     </div>
     </Router>
   );
+ }
 }
